11405169ee6cde02e5ae5ac7ebcc244ed14bd489
Vladimir Pochtarenko <wooldik94@gmail.com>
2020-10-24 21:02:29 +0300

    logic Button

--- 2tw/src/App.js
+++ 2tw/src/App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
 		return (<>
 			<h1>{this.state.header}</h1>
 			<input type="text" onChange={this.textInput}></input>
-			<Button btnTextClick={this.textButton} />
+			<Button btnTextClick={this.textButton} textValue={this.state.inputValue} />
 		</>)
 	}
 }
--- 2tw/src/components/Button.js
+++ 2tw/src/components/Button.js
@@ -1,14 +1,17 @@
+import { computeHeadingLevel } from '@testing-library/react';
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class Button extends Component {
 	state = {
-
+		textButton: `${this.props.textValue}`
 	}
 
 
 	render() {
 		return (<>
-			<button>Click!</button>
+			<button onClick={() => {
+				this.props.btnTextClick(this.state.textButton)
+			}}>{this.props.textValue === '' ? "Click" : this.props.textValue}</button>
 		</>)
 	}
 }
